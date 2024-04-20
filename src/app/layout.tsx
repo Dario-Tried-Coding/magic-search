bbd8cbb857df8731125ba2261f21f0632d1ab151
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2024-04-20 12:50:40 +0200

    shadcn ui init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/styles/globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@/styles/globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Magic Search | Dario Tried Coding",
-  description: "Mockup di un motore di ricerca realizzato con Next.js e Typescript.",
-};
+  title: 'Magic Search | Dario Tried Coding',
+  description: 'Mockup di un motore di ricerca realizzato con Next.js e Typescript.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="it">
+    <html lang='it'>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
